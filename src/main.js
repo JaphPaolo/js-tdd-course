379be467f8a13e242a70d2f1a218ae215d21b82d
JaphPaolo <japhet.nascimento@gmail.com>
2018-12-27 18:32:56 -0200

    Add good pratice

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const a = 3;
+const b = 2;
+const c = 'hahahahahaahahahahahaahahahahahaahahahahahaahahahahahaahahahahahaahahahahahaahahahahahaahahahahahaahahahahahaa';
+function sum() {
+  return a + b + c;
+}
+sum();
